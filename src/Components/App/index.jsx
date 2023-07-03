526ac9a672ab512d59a271e8fe98e35571e0b59d
rolando22 <rolando2207@gmail.com>
2023-07-03 01:06:20 -0300

    [UPDATE] estilos

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
-
 import { Cart, Header, Products } from '../';
 
 import { CartProvider } from './../../contexts/cart';
 
 import { useFilters } from './../../hooks/useFilters';
 
-import { products as initialProducts } from '../../mocks/products.json';
+import { products } from '../../mocks/products.json';
 
 import './App.css';
 
 export function App() {
-    const [products, setProducts] = useState(initialProducts);
     const { filterProducts } = useFilters();
 
     const filtedProducts = filterProducts({ products });
